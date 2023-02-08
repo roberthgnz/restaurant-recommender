442e751d07ae32501d653c1142072d8de8f904e2
rxb3rth <reliutg@gmail.com>
2023-02-08 19:56:40 +0100

    feat: get completion from api endpoint

--- api/generate.js
+++ api/generate.js
@@ -1,49 +1,53 @@
-require('dotenv').config();
-
-const { Configuration, OpenAIApi } = require("openai");
+import { OpenAIStream } from "../utils/OpenAIStream";
 
 async function getRestaurantReviews() {
-    const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJOzLmMImipBIRqApr2CgqrBQ&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+  const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJOzLmMImipBIRqApr2CgqrBQ&key=${process.env.GOOGLE_MAPS_API_KEY}`;
 
-    const response = await fetch(url, {
-        method: "GET",
-    });
+  const response = await fetch(url, {
+    method: "GET",
+  });
 
-    const data = await response.json();
+  const data = await response.json();
 
-    return data.result.reviews.map((review, index) => {
-        const text = review.text
-            .replace(/(\r\n|\n|\r)/gm, " ")
-            .replace(/[^a-zA-Z0-9 ]/g, "")
-            .replace(/\s+/g, " ")
-            .trim()
+  return data.result.reviews
+    .map((review, index) => {
+      const text = review.text
+        .replace(/(\r\n|\n|\r)/gm, " ")
+        .replace(/[^a-zA-Z0-9 ]/g, "")
+        .replace(/\s+/g, " ")
+        .trim();
 
-        return `Review ${index + 1}: ${text}`
-    }).join("\n");
-
+      return `Review ${index + 1}: ${text}`;
+    })
+    .join("\n");
 }
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
+export const config = {
+  runtime: "edge",
+};
 
-const openai = new OpenAIApi(configuration);
+const handler = async () => {
+  const reviews = await getRestaurantReviews();
 
-(async () => {
-    const reviews = await getRestaurantReviews()
+  const prompt =
+    "Based on this restaurant's reviews, do you think it is a good restaurant?\n\n" +
+    reviews;
 
-    const prompt = 'Based on this restaurant\'s reviews, do you think it is a good restaurant?\n\n' + reviews;
+  const payload = {
+    model: "text-davinci-003",
+    prompt,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 200,
+    stream: true,
+    n: 1,
+  };
 
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt,
-        temperature: 0.5,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        max_tokens: 200,
-        n: 1,
-    });
+  const stream = await OpenAIStream(payload);
 
-    console.log(response.data.choices);
-})();
+  return new Response(stream);
+};
+
+export default handler;
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from "vue";
+
+const result = ref("");
+
+const getCompletion = async () => {
+  const response = await fetch("/api/generate");
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  const data = response.body;
+  if (!data) {
+    return;
+  }
+
+  const reader = data.getReader();
+  const decoder = new TextDecoder();
+
+  let done = false;
+
+  while (!done) {
+    const { value, done: doneReading } = await reader.read();
+    done = doneReading;
+    const chunkValue = decoder.decode(value);
+    result.value += chunkValue;
+  }
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <p>{{ result }}</p>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
