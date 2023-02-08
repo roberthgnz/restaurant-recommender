ec90e7df8b1a8a4d6f270cd56ecbcaf3ceac11dc
rxb3rth <reliutg@gmail.com>
2023-02-08 19:58:03 +0100

    feat: install tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.vue", "./public/**/*.html", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
