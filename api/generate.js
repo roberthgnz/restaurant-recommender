eacaecc45ce29a8f1d755ee2c4b4539fe7a83186
rxb3rth <reliutg@gmail.com>
2023-02-08 23:28:00 +0100

    fix: read env variable

--- api/generate.js
+++ api/generate.js
@@ -5,7 +5,9 @@ function getLastNElements(array, n) {
 }
 
 async function getRestaurantReviews(place) {
-  const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place.place_id}&key=${process.env.VITE_GOOGLE_MAPS_API_KEY}`;
+  const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${
+    place.place_id
+  }&key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`;
 
   const response = await fetch(url, {
     method: "GET",
