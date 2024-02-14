c9eb908df27f35a81bd107a8da51904d40ff6185
Roberth González <reliutg@gmail.com>
2024-02-14 16:16:40 +0100

    Refactor Recommendations.vue to use markdown rendering

--- src/components/Recommendations.vue
+++ src/components/Recommendations.vue
@@ -1,7 +1,14 @@
 <script setup>
-defineProps(['value'])
+import { computed } from "vue";
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+
+const props = defineProps(['value'])
+
+const html = computed(() => {
+    return marked(props.value)
+})
 </script>
 
 <template>
-    <p class="shadow p-4 rounded-xl text-gray-500 mt-4">{{ value }}</p>
+    <div class="shadow p-4 rounded-xl text-gray-500 mt-4" v-html="html"></div>
 </template>
